0de1b8ee00da0c2c6ae6d3132ad4da23670b8957
shakil1995 <na.shakil1995@gmail.com>
2022-02-23 15:09:22 +0600

    add datatable

--- resources/views/categories/index.blade.php
+++ resources/views/categories/index.blade.php
@@ -3,9 +3,44 @@
 @section('content')
 
 
-<h1>this is categories page</h1>
-
-
+<!-- Content Header (Page header) -->
+<div class="content-header">
+    <div class="container-fluid">
+      <div class="row mb-2">
+        <div class="col-sm-6">
+          <h1 class="m-0">  Categories List </h1>
+        </div><!-- /.col -->
+        <div class="col-sm-6">
+          <ol class="breadcrumb float-sm-right">
+            <li class="breadcrumb-item"><a href="{{ route('dashboard') }}">Home</a></li>
+            <li class="breadcrumb-item active">Category List </li>
+          </ol>
+        </div><!-- /.col -->
+      </div><!-- /.row -->
+    </div><!-- /.container-fluid -->
+  </div>
+  <!-- /.content-header -->
+<table id="datatable" class="display table table-bordered" style="width:100%">
+    <thead>
+        <tr>
+            <th>SL NO</th>
+            <th>Category Name</th>
+            <th>Action</th>
+            
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>Tiger Nixon</td>
+            <td>System Architect</td>
+            <td>Edinburgh</td>
+        </tr>
+       
+       
+        
+    </tbody>
+  
+</table>
 
 
 @endsection
