5fb32b914662b6717340af170d904cb2c5afdd5b
shakil1995 <na.shakil1995@gmail.com>
2022-02-25 01:52:55 +0600

    sizes section add

--- resources/views/layouts/partials/_sidebar.blade.php
+++ resources/views/layouts/partials/_sidebar.blade.php
@@ -53,6 +53,12 @@
             </a>
           </li>
           <li class="nav-item">
+            <a href="{{ route('brands.index') }}" class="nav-link">
+              <i class="far fa-circle nav-icon"></i>
+              <p>Size</p>
+            </a>
+          </li>
+          <li class="nav-item">
             <a href="#" class="nav-link">
               <i class="nav-icon fas fa-th"></i>
               <p> Logout </p>
