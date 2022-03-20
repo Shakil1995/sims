3ee2b088af25c14a4fa4262568d40e6a9531edf2
shakil1995 <na.shakil1995@gmail.com>
2022-03-20 10:16:38 +0600

    store side bar added

--- resources/views/layouts/partials/_sidebar.blade.php
+++ resources/views/layouts/partials/_sidebar.blade.php
@@ -39,6 +39,13 @@
               <p>  Dashboard  </p>
             </a>  
           </li>
+
+          <li class="nav-item">
+            <a href="#" class="nav-link">
+              <i class="far fa-circle nav-icon"></i>
+              <p>Sotre Name</p>
+            </a>
+          </li>
           
           <li class="nav-item">
             <a href="{{ route('categories.index') }}" class="nav-link">
---
+++ resources/views/stores/index.blade.php
@@ -0,0 +1,65 @@
+@extends('layouts.mastar')
+
+@section('content')
+
+
+<!-- Content Header (Page header) -->
+<div class="content-header">
+    <div class="container-fluid">
+      <div class="row mb-2">
+        <div class="col-sm-6">
+          <h1 class="m-0">  Categories List </h1>
+        </div><!-- /.col -->
+        <div class="col-sm-6">
+          <ol class="breadcrumb float-sm-right">
+            <li class="breadcrumb-item"><a href="{{ route('dashboard') }}">Home</a></li>
+            <li class="breadcrumb-item active">Categry List</li>
+          </ol>
+        </div><!-- /.col -->
+      </div><!-- /.row -->
+    </div><!-- /.container-fluid -->
+  </div>
+ <a href="{{ route('categories.create') }}"  class="btn btn-success mb-3">Add Category</a>
+
+ <example-component> </example-component>
+
+
+<table id="datatable" class="display table-sm table-bordered " style="width:100%">
+    <thead>
+        <tr class="text-center">
+            <th>SL NO</th>
+            <th>Category Name</th>
+            <th>Action</th>
+            
+        </tr>
+    </thead>
+    <tbody>
+
+        @if ($categories)
+        @foreach ($categories as $key=>$category)
+        <tr class="text-center">
+            <td><b>{{ ++$key }}</b></td>
+            <td>{{ $category->name }}</td>
+            <td >
+                <a href="{{ route('categories.edit',$category->id) }}" class="btn btn-sm btn-info"> <i class="fa fa-edit"></i></a>
+                <a href="javascript:;" class="btn btn-sm btn-danger sa-delete" data-form-id="category-delete-{{ $category->id }}"> <i class="fa fa-trash"></i></a>
+            
+                <form id="category-delete-{{ $category->id }}" action="{{ route('categories.destroy',$category->id) }}" method="post">
+                @csrf
+            @method('DELETE')
+            </form>
+            </td>
+        </tr>
+        @endforeach
+            
+        @endif
+       
+       
+       
+        
+    </tbody>
+  
+</table>
+
+
+@endsection
